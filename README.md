94a17fc2e2000ca1200d90b08df806c384cae034
Christian Petersson <Christian.Petersson@cristie.se>
2017-10-17 13:07:44 +0200

    Start working with Automatic Installation Script for HPS

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Client_Install
-HPS Client Install via Powershell
+# Client Install
+To get this working we need to do following steps
+
+
+# Install ISP Client
+1. Find out what version of Operating System
+2. Does the ISP Client already been installed or not
+3. Install / Upgrade the Client
+4. Copy modify dsm.sys
+5. Run dsmc to verify we can access ISP Server
+5. Register the client Schedules
+6. Verify we got a Schedule
+
+# Install SQL Client
+
+# Install Exchange Client
+
---
+++ install.ps1
@@ -0,0 +1,65 @@
+########################
+## Configuration Area ##
+########################
+
+$ISPInstallPath = "C:\Temp\TSM\BAClient"
+$ISPInstallFile = "setup.txt"
+
+
+Function Get-DefaultVariables {
+    echo ""
+    echo ""
+    $ISP = "IBM Spectrum Protect"
+}
+
+Function Get-OSVersion {
+    echo "Check what version of Operating Systems you are running..."
+    echo "Please Wait..."
+
+    $osversion = (Get-WmiObject -class Win32_OperatingSystem).Caption
+    $os64or32bit = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+    $nodename = (Get-WmiObject Win32_OperatingSystem).CSName
+
+    echo "You are running version: $osversion on $os64or32bit"
+    echo ""
+    echo ""
+}
+
+Function Get-ISPClientExist {
+    echo "Check if IBM Spectrum Protect Backup-Archive Client exist"
+    echo "Please Wait..."
+
+    $ISPClientExistVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\IBM\ADSM\CurrentVersion\BackupClient" -Name PtfLevel).PtfLevel
+    if (test-path "HKLM:\SOFTWARE\IBM\ADSM\CurrentVersion\BackupClient") {
+        echo "You have already a $ISP Client Installed version: $ISPClientExistVersion"
+        $ISPBAClientExist = $True
+    }
+    else {
+        echo "$ISP Backup-Archive Client does not exist"
+        $ISPBAClientExist = $False
+    }
+    echo ""
+    echo ""
+}
+
+Function Get-ISPInstallPath {
+    echo "Check if you the path to $ISP Client Installation Files exist"
+
+    if (-not (test-path -path "$ISPInstallPath\$ISPInstallFile")) {
+        echo "This file should exist"
+    }
+    else {
+        echo "Found $ISP Backup-Archive Client Installer"
+        echo "Found the installations files under directory $ISPInstallPath"
+    }
+    echo ""
+    echo ""
+}
+
+Get-DefaultVariables
+Get-OSVersion
+Get-ISPClientExist
+Get-ISPInstallPath
+
+
+
